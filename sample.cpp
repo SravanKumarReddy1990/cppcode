48d703ff0fd4db5419ef5bd4a151ad30d69b6d6f
SravanKumarReddy1990 <bskreddy1990@gmail.com>
2022-12-14 12:36:56 +0530

    code changes by data.h

--- sample.cpp
+++ sample.cpp
@@ -1,31 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <string.h>  
-class Data
-{
-    private:
-        Data(){};
-        string key;
-        string value;
-    public:
-        Data(string key,string value)
-        {
-          this->key=key;
-          this->value=value;
-        }
-        string getKey(){return this->key;}        
-        string getValue(){return this->value;}
-        void setKey(string key){this->key=key;}
-        
-        void setValue(string value){this->value=value;}
-        void printNo()
-        {
-          std::cout << key << std::endl;
-        }
-};
-
-
-
+#include "data.h"  
 
 
 int main()
